e7e8d8a7cacbc7dff5a411dcdabfee28cb39cfc9
DKJazzu <dkjazzu@gmail.com>
2026-02-02 17:36:09 +0200

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -86,7 +86,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, removedPerson) => {
+    if (err) return done(err);
+    done(null, removedPerson);
+  });
 };
 
 const removeManyPeople = (done) => {
